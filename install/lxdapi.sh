33269bcd61f8f17b65a6bfd228ad2a0110911155
欢 <2711452309@qq.com>
2025-10-16 12:36:43 +0800

    update

--- install/lxdapi.sh
+++ install/lxdapi.sh
@@ -3,7 +3,7 @@
 RED='\033[0;31m'; GREEN='\033[0;32m'; YELLOW='\033[1;33m'
 BLUE='\033[0;34m'; NC='\033[0m'
 
-REPO="https://github.com/xkatld/zjmf-lxd-server"
+REPO="https://github.com/StarVM-OpenSource/zjmf-lxd-server-v1"
 VERSION=""
 NAME="lxdapi"
 DIR="/opt/$NAME"
